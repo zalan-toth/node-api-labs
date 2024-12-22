6b1681a17d36270506b9bbeba9b8ac7d4e98011c
Zalán Tóth <zalant.tsc@gmail.com>
2024-12-22 22:44:12 +0100

    Routes for get task and task details

---
+++ tasky-api/api/tasks/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { tasksData } from './tasksData';
+
+const router = express.Router(); 
+
+router.get('/', (req, res) => {
+    res.json(tasksData);
+});
+
+// Get task details
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    const task = tasksData.tasks.find(task => task.id === id);
+    if (!task) {
+        return res.status(404).json({ status: 404, message: 'Task not found' });
+    }
+    return res.status(200).json(task);
+});
+
+
+export default router;
