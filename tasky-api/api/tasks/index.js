6d6a5d39ad2035e20d0e10eb9c8027a86427bd7f
Zalán Tóth <zalant.tsc@gmail.com>
2024-12-22 22:50:25 +0100

    added routing for posting a new task

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import express from 'express';
 import { tasksData } from './tasksData';
 
@@ -15,6 +16,22 @@ router.get('/:id', (req, res) => {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
     return res.status(200).json(task);
+});
+
+//Add a task
+router.post('/', (req, res) => {
+    const { title, description, deadline, priority, done } = req.body;
+    const newTask = {
+        id: uuidv4(),
+        title,
+        description,
+        deadline,
+        priority,
+        done
+    };
+    tasksData.tasks.push(newTask);
+    res.status(201).json(newTask);
+    tasksData.total_results++;
 });
 
 
