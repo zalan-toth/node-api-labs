c9fd5642eba70e7ef06c6376cebf87f3030c52ce
Zalán Tóth <zalant.tsc@gmail.com>
2024-12-23 01:49:46 +0100

    Salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
 
 const Schema = mongoose.Schema;
 
@@ -14,5 +16,29 @@ const regexValidator = (value) => {
   
   UserSchema.path("password").validate(regexValidator, "Invalid password format. Make it stronger!");
   
-  
+UserSchema.methods.comparePassword = async function (passw) { 
+    return await bcrypt.compare(passw, this.password); 
+}
+
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10; // You can adjust the number of salt rounds
+  //const user = this;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+  } catch (error) {
+     next(error);
+  }
+
+  } else {
+      next();
+  }
+});
+
 export default mongoose.model('User', UserSchema);
