63c57f3ab74545f324dc0cda2a8b857dce8fe14d
Zalán Tóth <zalant.tsc@gmail.com>
2024-12-23 02:37:54 +0100

    Integration Lab - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,10 +1,13 @@
 export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=c79cdbf4f527a182a7b7331c31ab2a47&language=en-US&include_adult=false&page=1`
-    )
-    return response.json()
-  };
-
+  const response = await fetch(
+    'http://localhost:8080/api/movies', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  }
+  )
+  return response.json();
+};
 
 export const login = async (username, password) => {
     const response = await fetch('http://localhost:8080/api/users', {
