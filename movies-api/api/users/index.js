4ea5871eb2f3cbf813df0c3463d4ee60193997b0
Zalán Tóth <zalant.tsc@gmail.com>
2024-12-23 01:55:04 +0100

    Added JWT

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import User from './userModel';
 import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+
 
 const router = express.Router(); // eslint-disable-line
 
@@ -12,20 +14,19 @@ router.get('/', async (req, res) => {
 
 // register(Create)/Authenticate User
 router.post('/', asyncHandler(async (req, res) => {
-    if (req.query.action === 'register') {  //if action is 'register' then save to DB
-        await User(req.body).save();
-        res.status(201).json({
-            code: 201,
-            msg: 'Successful created new user.',
-        });
-    }
-    else {  //Must be an authenticate then!!! Query the DB and check if there's a match
-        const user = await User.findOne(req.body);
-        if (!user) {
-            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
-        }else{
-            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+    try {
+        if (!req.body.username || !req.body.password) {
+            return res.status(400).json({ success: false, msg: 'Username and password are required.' });
         }
+        if (req.query.action === 'register') {
+            await registerUser(req, res);
+        } else {
+            await authenticateUser(req, res);
+        }
+    } catch (error) {
+        // Log the error and return a generic error message
+        console.error(error);
+        res.status(500).json({ success: false, msg: 'Internal server error.' });
     }
 }));
 
@@ -41,6 +42,28 @@ router.put('/:id', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to Update User' });
     }
 });
+
+
+async function registerUser(req, res) {
+    // Add input validation logic here
+    await User.create(req.body);
+    res.status(201).json({ success: true, msg: 'User successfully created.' });
+}
+
+async function authenticateUser(req, res) {
+    const user = await User.findByUserName(req.body.username);
+    if (!user) {
+        return res.status(401).json({ success: false, msg: 'Authentication failed. User not found.' });
+    }
+
+    const isMatch = await user.comparePassword(req.body.password);
+    if (isMatch) {
+        const token = jwt.sign({ username: user.username }, process.env.SECRET);
+        res.status(200).json({ success: true, token: 'BEARER ' + token });
+    } else {
+        res.status(401).json({ success: false, msg: 'Wrong password.' });
+    }
+}
 
 
 export default router;
